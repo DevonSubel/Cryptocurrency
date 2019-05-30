d4d70169624d5641b2de69006e707307b9dc3005
Carlos <josesagrero506@gmail.com>
2019-05-30 13:41:02 -0700

    added more stuff to driver.py

--- driver.py
+++ driver.py
@@ -1,6 +1,27 @@
-# Devon making the input files. reading input files
-# For every transaction in file, instantiate transaction object
-# Add object to unverified transaction pool
+
+
+class TransactionPools(self):
+    # Holds unverified and verified trasaction pools 
+    # Once a transaction has been verified, move it to the verified pool
+
+
+    # unverifiedPool = () //list for holding verified transactions
+    # verifiedPool = ()   //list for holding verified transactions
+    
+    def newTransaction(self):
+        # when called, read transaction from file and add it to unverified pool 
+        # For each transaction, instantiate transaction object and add it to the pool
+
+    
+    def getTransaction(self):
+        # return transaction from unverified pool
+    
+    def markTransactionAsInvalid(self):
+        # 
+
+    # def verifyTransaction -> maybe. mark certain transaction as verified. 
+    # & move it to the verified pool 
+
 
 
 class driver(self):
@@ -14,16 +35,29 @@ class driver(self):
 
     # create genesis block ie initial blockchain 
 
-    # Read intput file and create unverified pool (probably an array)
-    # Make sure that unverified pool data struct can be read by all nodes
 
-    # Launch numNodes number of nodes nodes. pass in initial difficulty, 
+    # instantiate transaction pools
+    # Make sure that unverified pool data struct can be read by all nodes
+    pools = TransactionPools()
+
+    
+    # Launch numNodes number of nodes. pass in initial difficulty, pool reference
+    # Make sure that pool reference can be modified by the nodes. 
+    # Figure out how first node will get ledger. probably pass in genesis block to every node
+    # Per the spec, we might need to be able to set the intent of a node: malicious vs cooperative
+
+
+    while True:
+        # Call pool.newTransaction() at random times to add transactions to unverified pool
+        # maybe communicate with nodes in order to print out current status of blockchain
+        # and to print out when new blocks have been solved 
+
 
 
 
 class node(object):
 
-    def getLedgerFromNetwork():
+    def getLedgerFromNetwork(self):
         print("getting current ledger from network")
         # Each node will have to get a copy of the ledger
         # Figure out how to get current ledger for new node entering network
@@ -41,16 +75,30 @@ class node(object):
         # return true if transaction is valid
 
     
-    def solvedPuzzle(block, random, difficulty):
+    def solvedPuzzle(block, randomVal, difficulty):
         # Run proof of work
-        # hash the block with iterator until the hash has a certain number of zeroes. denoted by difficulty
+        # hash the block with randomVal until the hash has n number of zeroes. denoted by difficulty argument
         # if the puzzle is solved, return the block + iterator value and the hash. this is the new block
 
     def createBaseBlock(transaction):
         # creates base block from transaction to run it through the puzzle 
 
-    def blockSolved():
+    def newBlockSolvedByNetwork(self):
         # Check the node network to see if a block  has aleady been solved. 
+        # will  probably get updated ledger or maybe just new block
+
+        # return true if new block
+
+    def broadcastNewBlock(self):
+        # Broadcast the blocks to other nodes since theyre threads in same program
+        # Figure out how to do this
+        
+
+
+    # Receive blocks from other nodes and update local ledger to 
+    def receiveNewBlocks(self):
+        print("checking for new solved blocks broadcast to newtwork")
+
 
 
     def mineBlock(self):
@@ -71,37 +119,7 @@ class node(object):
                 receiveNewBlocks()
                 return 
 
-    # need to broadcast the blocks to other nodes 
-    def broadcastNewBlock(self):
-        print("SOlved puzzle. Broadcasting new block")
-
-
-    # Receive blocks from other nodes and update local ledger to 
-    def receiveNewBlocks(self):
-        print("checking for new solved blocks broadcast to newtwork")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     
-
-
 arr = [1] * 10
 
 nd = node()

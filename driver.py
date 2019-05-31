31940d5692687e5124ca6687c7fb330d48f826d4
Carlos <josesagrero506@gmail.com>
2019-05-30 17:24:58 -0700

    added multithreading tests

--- driver.py
+++ driver.py
@@ -1,28 +1,26 @@
-import threading 
+from threading import Thread
 import os
 # from node import *
 # from transaction import *
 # from transactionPool import *
 # from blockChain import *
+class testNode(object):    
+    def run(self, arr):
+        for a in arr: 
+            print(a)   
 
+        for i in range(len(arr)):
+            arr[i] = 11
+        
 
-class testNode(object):
-    def __init__(self, array):
-        arr = array
-    
-    for a in arr: 
-        print(a)
-
-    
-
-class driver(object):
+class Driver(object):
     
     def __init__(self, numNodes, transactionsFile):
         self.numNodes = numNodes
         self.transactionsFile = transactionsFile
-        # self.pool = TransactionPools(transactionsFile)
+        # self.pool = TransactionPools(transactionsFile
 
-
+    
     def run(self):
         # Initiate a simulation with n numNodes 
 
@@ -38,10 +36,17 @@ class driver(object):
         # for node in range(self.numNodes):
         array = [10] * 10
 
+        testND = testNode()
+
         print("hello world")
 
+        if __name__ == "__main__": 
+            for t in range()
+            t1 = Thread(target=testND.run(array), name='t1')
+            t2 = Thread(target=testND.run(array), name='t2')
 
-
+            t1.start()
+            t2.start()
 
         # Launch numNodes number of nodes. pass in pool reference
         # Make sure that pool reference can be modified by the nodes. 
@@ -51,15 +56,15 @@ class driver(object):
 
         # while True:
             # Call pool.newTransaction() at random times to add transactions to unverified pool
+            # Print out state of blockchain by reading verified transaction pool which holds
+            # verified blocks 
             # maybe communicate with nodes in order to print out current status of blockchain
             # and to print out when new blocks have been solved 
 
 
-
-
 # pool = Transaction(transactionFile)
 
-driver = driver(10, "transaction file")
+driver = Driver(10, "transaction file")
 
 driver.run()
 

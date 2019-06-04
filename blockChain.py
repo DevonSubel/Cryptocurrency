03b1bc436861d32215fa548b64d221237855c9e0
svmadiraju <saisujit25@gmail.com>
2019-06-04 15:57:43 -0700

    Fix

--- blockChain.py
+++ blockChain.py
@@ -1,55 +1,3 @@
-<<<<<<< HEAD
-import Crypto 
-import hashlib
-import transaction
-
-
-class block(object):
-    # Block contains
-        # single transaction
-        # hash of previous block
-        # proof of work
-    
-    def __init__(self, transaction):
-        self.transaction = transaction
-        self.prevBlockHash = ''
-        self.proofOfWork = ''
-
-    def blockToStr(self):
-        transactionStr = self.transaction.toString()
-        fieldsStr = self.prevBlockHash + self.proofOfWork
-        blockStr = transactionStr + fieldsStr
-        return(blockStr)
-
-    def hashBlock(self):
-        blockStr = self.blockToStr()
-        blockHash = hashlib.sha256(blockStr).digest()
-        return blockHash
-        
-        
-
-class blockchain(object):
-
-    # add blocks to some data structure. list is probably best 
-    blockchainList = []
-
-    def addToBlockchain(self, block):
-        if len(block) != 0:
-            prevblock = self.blockchainList[len(self.blockchainList) - 1]
-            prevhash = prevblock.proofOfWork
-            block.prevBlockHash = prevhash
-        self.blockchainList.append(block)
-
-    def getBlockchain(self):
-        return self.blockchainList
-
-    def getBlockchainLength(self):
-        return len(self.blockchainList)
-
-
-
-
-=======
 import Crypto 
 import hashlib
 import transaction
@@ -96,4 +44,3 @@ class blockchain(object):
 
 
 
->>>>>>> 9cb9da0bf1bc980df6e1d46f5dcdca4c387873de

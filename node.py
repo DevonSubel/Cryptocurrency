677edd4c78a64feac9918ef1cf5c6885826fda2c
Devon Subel <subel.devon@gmail.com>
2019-06-05 17:02:38 -0700

    gitignore fix

--- node.py
+++ node.py
@@ -1,3 +1,6 @@
+import math
+from mersenneProofOfWork import *
+
 class node(object):
 
     import hashlib
@@ -110,6 +113,76 @@ class node(object):
             return block, nonce, hval 
         else:
             return "", "", ""
+
+    def mersenneWork(n, prime):
+        # Initialize all entries of boolean
+        # array as true. A value in prime[i]
+        # will finally be false if i is Not
+        # a prime, else true bool prime[n+1]
+        for i in range(0, n + 1):
+            prime[i] = True
+
+        p = 2
+        while (p * p <= n):
+
+            # If prime[p] is not changed,
+            # then it is a prime
+            if (prime[p] == True):
+
+                # Update all multiples of p
+                for i in range(p * 2, n + 1, p):
+                    prime[i] = False
+
+            p += 1
+
+    #Call me to generate primes
+    def mersennePrimes(n):
+        # Create a boolean array
+        # "prime[0..n]"
+        prime = [0] * (n + 1)
+
+        # Generating primes using Sieve
+        mersenneWork(n, prime)
+
+        # Generate all numbers of the
+        # form 2^k - 1 and smaller
+        # than or equal to n.
+        k = 2
+        while (((1 << k) - 1) <= n):
+
+            num = (1 << k) - 1
+
+            # Checking whether number
+            # is prime and is one
+            # less then the power of 2
+            if (prime[num]):
+                # print(num)
+                largePrime = num
+
+            k += 1
+        return largePrime, math.log(largePrime + 1, 2) #Returns mersenne prime, exponent to generate it
+
+    def isPrime(num):
+        if num > 1:
+            # Iterate from 2 to n / 2
+            for i in range(2, num // 2):
+
+                # If num is divisible by any number between
+                # 2 and n / 2, it is not prime
+                if (num % i) == 0:
+                    return False
+            else:
+                return True
+        else:
+            return False
+
+    #I Verify to make sure the puzzle is done correctly
+    def verify(n, num):
+        print(num)
+        print(2 ** n - 1)
+        if (2 ** n - 1 != num):
+            return False
+        return isPrime(num)
 
 
 

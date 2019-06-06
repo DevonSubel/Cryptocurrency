7d4cf53061794e95c43cc8e10e3c921af7619f55
moomme <momme_omer@hotmail.com>
2019-06-06 21:22:49 +0200

    Complete TransactionMaker

--- transactionMaker.py
+++ transactionMaker.py
@@ -106,12 +106,12 @@ data[3].append({
    "SIGNATURE": sig3
 })
 
-# TRANSFER: VK3 MERGES TRANS 2 + 3, GIVES VK4 3
+# TRANSFER: VK3 MERGES TRANS 2 + 3, GIVES VK4 3, VK3 has 4 left
 
 data[4] = []
 tinput4 = [[number2, 0], [number3, 0]]
 toutput4 = [[3, vk4.to_string().encode('hex')], [4, vk3.to_string().encode('hex')]]
-ttype4 = 'TRANS'
+ttype4 = 'MERGE'
 sig4 = [sk3.sign(str(tinput4[0]) + str(toutput4) + ttype4).encode("hex"), sk3.sign(str(tinput4[1]) + str(toutput4) + ttype4).encode("hex")] 
 
 number4 = hashlib.sha256(str(tinput4) + str(toutput4) + str(sig4)).hexdigest()
@@ -122,6 +122,95 @@ data[4].append({
    'INPUT': tinput4,
    'OUTPUT': toutput4,
    "SIGNATURE": sig4
+})
+
+#TRANSFER: VK3 and VK2 JOINS (7) to give VK5 3, VK2 has 2 left, VK3 has 2 left
+
+data[5] = []
+tinput5 = [[number4, 1], [number3, 1]]
+toutput5 = [[3, vk5.to_string().encode('hex')], [2, vk2.to_string().encode('hex')], [2, vk3.to_string().encode('hex')]]
+ttype5 = 'JOIN'
+sig5 = [sk3.sign(str(tinput5[0]) + str(toutput5) + ttype5).encode("hex"), sk2.sign(str(tinput5[1]) + str(toutput5) + ttype5).encode("hex")] 
+
+number5 = hashlib.sha256(str(tinput5) + str(toutput5) + str(sig5)).hexdigest()
+
+data[5].append({
+   'NUMBER': number5,
+   'TYPE': ttype5,
+   'INPUT': tinput5,
+   'OUTPUT': toutput5,
+   "SIGNATURE": sig5
+})
+
+# TRANSFER: VK1 GIVES 3 -> VK2, HAS 12 LEFT
+
+data[6] = []
+tinput6 = [[number2, 1]]
+toutput6 = [[3, vk2.to_string().encode('hex')], [12, vk1.to_string().encode('hex')]]
+ttype6 = 'TRANS'
+sig6 = [sk1.sign(str(tinput6[0]) + str(toutput6) + ttype6).encode("hex")] 
+
+number6 = hashlib.sha256(str(tinput6) + str(toutput6) + str(sig6)).hexdigest()
+
+data[6].append({
+   'NUMBER': number6,
+   'TYPE': ttype6,
+   'INPUT': tinput6,
+   'OUTPUT': toutput6,
+   "SIGNATURE": sig6
+})
+
+# TRANSFER: VK1 GIVES 3 -> VK3, HAS 9 LEFT (May be double spend)
+
+data[7] = []
+tinput7 = [[number6, 1]]
+toutput7 = [[3, vk2.to_string().encode('hex')], [9, vk1.to_string().encode('hex')]]
+ttype7 = 'TRANS'
+sig7 = [sk1.sign(str(tinput7[0]) + str(toutput7) + ttype7).encode("hex")] 
+
+number7 = hashlib.sha256(str(tinput7) + str(toutput7) + str(sig7)).hexdigest()
+
+data[7].append({
+   'NUMBER': number7,
+   'TYPE': ttype7,
+   'INPUT': tinput7,
+   'OUTPUT': toutput7,
+   "SIGNATURE": sig7
+})
+
+# Double spend maybe Gives 3 to vk4, Has 9 left
+data[8] = []
+tinput8 = [[number6, 1]]
+toutput8 = [[3, vk4.to_string().encode('hex')], [9, vk1.to_string().encode('hex')]]
+ttype8 = 'TRANS'
+sig8 = [sk1.sign(str(tinput8[0]) + str(toutput8) + ttype8).encode("hex")] 
+
+number8 = hashlib.sha256(str(tinput8) + str(toutput8) + str(sig8)).hexdigest()
+
+data[8].append({
+   'NUMBER': number8,
+   'TYPE': ttype8,
+   'INPUT': tinput8,
+   'OUTPUT': toutput8,
+   "SIGNATURE": sig8
+})
+
+# TRANSFER: VK2 GIVES 1 -> VK5, HAS 2 LEFT
+
+data[9] = []
+tinput9 = [[number6, 0]]
+toutput9 = [[1, vk5.to_string().encode('hex')], [2, vk2.to_string().encode('hex')]]
+ttype9 = 'TRANS'
+sig9 = [sk2.sign(str(tinput9[0]) + str(toutput9) + ttype9).encode("hex")] 
+
+number9 = hashlib.sha256(str(tinput9) + str(toutput9) + str(sig9)).hexdigest()
+
+data[9].append({
+   'NUMBER': number9,
+   'TYPE': ttype9,
+   'INPUT': tinput9,
+   'OUTPUT': toutput9,
+   "SIGNATURE": sig9
 })
 
 with codecs.open('sampleInput.json','w', 'utf8') as outfile:

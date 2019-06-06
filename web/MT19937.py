dd3660150d7952b887d30396d7b8d58ad10940a9
svmadiraju <saisujit25@gmail.com>
2019-06-05 18:19:37 -0700

    web

---
+++ web/MT19937.py
@@ -0,0 +1,15 @@
+
+#Mersenne Twister MT 19937
+class MT19937:
+	def __init__(self, seed):
+		#TODO: Initialize MT state here
+		return
+
+	def extract_number(self):
+		#TODO: Temper and Extract Here
+		return 42
+
+	def generate_number(self):
+		#TODO: Mix state here
+		return
+
